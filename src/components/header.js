ec45cc66356b02032ad7e53693aec88368dcdfc9
enun03 <enunez025@gmail.com>
2018-07-15 15:37:03 -0400

    dev wip

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+var renderHeader = function () {
+  var header = document.querySelector('#page-header');
+
+  header.innerHTML = `
+  <div class="jumbotron">
+    <h1 class="display-4">Magnificat</h1>
+    <p class="lead">Health Services & Insurance</p>
+  </div>
+  `;
+}()
