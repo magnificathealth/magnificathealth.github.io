4e445b77581981e4103ae1c3c01bdf3dc6ca6f19
enun03 <enunez025@gmail.com>
2018-07-19 23:21:40 -0400

    Updating Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,7 +12,7 @@ var renderNavbar = function () {
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="/">Inicio <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="/">Inicio</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/proposito.html">Prop&oacute;sito</a>
