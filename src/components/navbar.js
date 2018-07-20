d83c1d00cfb58249b30f49f868da523436a1e315
enun03 <enunez025@gmail.com>
2018-07-19 23:19:58 -0400

    Adding proposito and services

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,11 @@
 var renderNavbar = function () {
   var nav = document.getElementById('page-nav');
   nav.innerHTML = `
-    <nav class="navbar fixed-top navbar-expand-md navbar-light bg-light">
-    <a class="navbar-brand" href="#"></a>
+    <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="/">
+      <img src="src/img/logo/magnificat_logo.png" width="36px" height="36px" />
+      Magn&iacute;ficat
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -12,11 +15,14 @@ var renderNavbar = function () {
           <a class="nav-link" href="/">Inicio <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/index.html#servicios">Servicios</a>
+          <a class="nav-link" href="/proposito.html">Prop&oacute;sito</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/conocenos.html">Con&oacute;cenos</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/servicios.html">Servicios</a>
+        </li>        
         <li class="nav-item">
           <a class="nav-link" href="src/docs/Brochure_MHS&I.pdf">Brochure</a>
         </li>
